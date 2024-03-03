cb22cbe51648dfd50ea33e2d871fde0ced4a3e7b
Marcio <marciodosanjos@ymail.com>
2024-03-03 14:32:22 -0300

    first project configs

---
+++ server.js
@@ -0,0 +1,8 @@
+import http from 'http';
+import app from './app/app.js';
+
+
+// create the server
+const PORT = process.env.PORT || 7000;
+const server = http.createServer(app);
+server.listen(PORT, console.log(`Server is running on port ${PORT}`))
